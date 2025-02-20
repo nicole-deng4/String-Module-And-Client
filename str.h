9eafdb78933daf3cb300a1f32903c5f65648ada5
nicole-deng4 <nd6753@princeton.edu>
2025-02-20 13:47:06 -0500

    fixing comments

--- str.h
+++ str.h
@@ -3,19 +3,19 @@
 
 #include <stddef.h>
 
-# As long as str isn't null, this function returns the length of the string pointed to by str
+/* As long as str isn't null, this function returns the length of the string pointed to by str */
 size_t Str_getLength (const char str*);
 
-# As long as str isn't null, this function copies the string pointed to by str2 into the array pointed by str1
+/* As long as str isn't null, this function copies the string pointed to by str2 into the array pointed by str1 */
 char *Str_copy (const char *str1, const char *str2);
 
-# As long as str1 and str2 aren't null and str1 holds enough space to accommodate the additional characters from str2, this function appends the string pointed to by str2 to the end of the string pointed to by str1 and returns the pointer to str1. 
+/* As long as str1 and str2 aren't null and str1 holds enough space to accommodate the additional characters from str2, this function appends the string pointed to by str2 to the end of the string pointed to by str1 and returns the pointer to str1. */ 
 char *Str_concat (const char *str1, const char *str2);
 
-#As long as str1 and str2 aren't null, this function compares str1 and str2, returning a positive integer if str1 > str2, zero if str1 = str2, and a negative integer if str1 < str2.
+/* As long as str1 and str2 aren't null, this function compares str1 and str2, returning a positive integer if str1 > str2, zero if str1 = str2, and a negative integer if str1 < str2. */
 int Str_compare (const char *str1, const char *str2);
 
-#As long as str1 and str2 aren't null, this function returns a pointer to the first occurrence of str2 in str1 or returns null if str2 was not found in str1. 
+/* As long as str1 and str2 aren't null, this function returns a pointer to the first occurrence of str2 in str1 or returns null if str2 was not found in str1. */
 char Str_search (const char *str1, const char *str2);
 
 #endif
