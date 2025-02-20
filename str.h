60c7a125a0b183a6b9e9547f5b573ba6e406e84e
nicole-deng4 <nd6753@princeton.edu>
2025-02-20 15:24:47 -0500

    Update str.h

--- str.h
+++ str.h
@@ -5,13 +5,13 @@
 
 #include <stddef.h>
 
-/* This function returns the length of the string pointed to by str, as long as str isn't null. */
+/* As long as str isn't null, this function returns the length of the string pointed to by str. */
 size_t Str_getLength (const char *str);
 
-/* This function copies the string pointed to by str2 into the array pointed by str1, as long as str isn't null. */
+/* As long as str isn't null, this function copies the string pointed to by str2 into the array pointed by str1 and returns the pointer to the updated str1. */
 char *Str_copy (char *str1, const char *str2);
 
-/* As long as str1 and str2 aren't null and str1 holds enough space to accommodate the additional characters from str2, this function appends the string pointed to by str2 to the end of the string pointed to by str1 and returns the pointer to str1. */ 
+/* As long as str1 and str2 aren't null and str1 holds enough space to accommodate the additional characters from str2, this function appends the string pointed to by str2 to the end of the string pointed to by str1 and returns the pointer to the updated str1. */ 
 char *Str_concat (char *str1, const char *str2);
 
 /* As long as str1 and str2 aren't null, this function compares str1 and str2, returning a positive integer if str1 > str2, zero if str1 = str2, and a negative integer if str1 < str2. */
