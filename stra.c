24ecf442391ebb34e13b816ae28386dcc0ca411c
nicole-deng4 <nd6753@princeton.edu>
2025-02-20 14:23:04 -0500

    Update stra.c

--- stra.c
+++ stra.c
@@ -28,7 +28,8 @@ char *Str_copy (char str1[], const char str2[])
 char *Str_concat (char str1[], const char str2[])
 {
   size_t str1i = 0;
-  size_t str2i = 0
+  size_t str2i = 0;
+  
   assert (str1 != NULL && str2 != NULL);
   while (str1[str1i] != '\0')
   {
