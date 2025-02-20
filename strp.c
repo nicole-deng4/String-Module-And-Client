a256cc5187aa5c9c2f634c25eca0d3da06e0bd8f
nicole-deng4 <nd6753@princeton.edu>
2025-02-20 16:35:50 -0500

    comments

--- strp.c
+++ strp.c
@@ -1,6 +1,13 @@
+/* String Module Pointer Notation Implementation:
+This function implements the basic string manipulation functions including Str_getLength, Str_copy, Str_concat, Str_compare, and 
+Str_search using pointer notation. 
+*/
+
 # include "str.h"
 # include <assert.h>
 
+/* This function computes the length of a given string using pointer notation by initializing a pointer to traverse the character sequence 
+and returning the number of characters before it reaches the null terminator. */
 size_t Str_getLength (const char *str)
 {
   const char *pc = str;
@@ -12,6 +19,9 @@ size_t Str_getLength (const char *str)
   return (size_t)(pc-str);
 }
 
+/* Assuming that str1 has enough space to store str2, this function copies the contents of str2 into str1 using pointer notation by moving
+a pointer through str2, copying each character into str1 until the null terminator is reached. It returns a pointer to the beginning of 
+str1. */
 char *Str_copy (char *str1, const char *str2)
 {
   char *newStr1 = str1;
@@ -25,6 +35,9 @@ char *Str_copy (char *str1, const char *str2)
   return newStr1;
 }
 
+/* Assuming that str1 has enough space to accommodate str2, this function appends str2 to the end of str1 using pointer notation by moving 
+a pointer to the null terminator of str1, then copying characters from str2 into str1 until the null terminator of str2 is reached. It 
+returns a pointer to the beginning of str1. */
 char *Str_concat (char *str1, const char *str2)
 {
   char *newStr1 = str1;
@@ -45,6 +58,9 @@ char *Str_concat (char *str1, const char *str2)
   return newStr1;
 }
 
+/* This function compares two strings lexicographically using pointer notation by traversing two character pointers, comparing corresponding 
+characters of str1 and str2 until a difference is found or the null terminator is reached. It returns a negative, zero, or positive integer 
+based on lexicographic order. */
 int Str_compare (const char *str1, const char *str2)
 {
   
@@ -57,6 +73,9 @@ int Str_compare (const char *str1, const char *str2)
   return (int)((*str1) - (*str2)); /* Unsigned char? */
 }
 
+/* This function searches for the first occurrence of str2 in str1 by using pointer notation to iterate through str1, checking for a 
+substring match by comparing characters in str2 to corresponding characters of str1. It returns a pointer to the first occurrence of str2 
+in str1, or NULL if not found. */
 char *Str_search (const char *str1, const char *str2)
 {
   
